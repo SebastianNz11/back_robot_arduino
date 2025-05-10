4f3cbe5ec21189908892e2811d8b9a02803b2054
Sebastian Nuñez <sebas11np@gmail.com>
2025-05-10 17:22:20 -0600

    b2

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ app.post('/temperatura', (req, res) => {
   const { valor } = req.body;
   if (typeof valor === 'number') {
     temperaturaActual = valor;
+    console.log(`Temperatura recibida: ${valor}°C`);
+    res.send('Temperatura guardada');
   } else {
     res.status(400).send('Formato incorrecto');
   }
